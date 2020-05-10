5513f3fb31b2c7b63e1c3f068e15344688a7a91c
jcg678 <jcg678@hotmail.es>
2020-05-10 11:38:32 +0200

    Login template

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
   public page_title: string;
-  constructor() {
+  public user:User;
+  public status: string;
+
+  constructor(private _userService : UserService) {
     this.page_title = 'Login';
+    this.user = new User(1,'','','','','ROLE_USER','');
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+    console.log(this.user);
   }
 
 }
