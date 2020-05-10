8ea8675a77ca9c8c208346adfd2238a07a601161
jcg678 <jcg678@hotmail.es>
2020-05-10 11:57:56 +0200

    Login user

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../../models/user';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../models/user';
 import {UserService} from '../../services/user.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +12,53 @@ import {UserService} from '../../services/user.service';
 export class LoginComponent implements OnInit {
 
   public page_title: string;
-  public user:User;
+  public user: User;
   public status: string;
+  public token: string;
+  public identity;
 
-  constructor(private _userService : UserService) {
+  constructor(private _userService: UserService) {
     this.page_title = 'Login';
-    this.user = new User(1,'','','','','ROLE_USER','');
+    this.user = new User(1, '', '', '', '', 'ROLE_USER', '');
   }
 
   ngOnInit() {
   }
 
-  onSubmit(form){
-    console.log(this.user);
+  onSubmit(form) {
+    this._userService.signup(this.user).subscribe(
+      response => {
+        this.status = 'success';
+
+        if (!response.status || response.status != 'error') {
+          this.status = 'success';
+          this.identity = response;
+          //token
+          this._userService.signup(this.user, true).subscribe(
+            response => {
+              this.status = 'success';
+              if (!response.status || response.status != 'error') {
+
+                this.token = response;
+                console.log(this.token);
+              }
+            },
+            error => {
+              this.status = 'error';
+              console.log('error');
+            }
+          );
+
+
+          console.log(this.identity);
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log('error');
+      }
+    );
   }
+
 
 }
