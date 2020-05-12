40d0b976af937ebbf21f30be81249d8268529ce6
jcg678 <jcg678@hotmail.es>
2020-05-12 14:25:16 +0200

    Formulario ajustes usuario

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
   public page_title:string;
-  constructor() {
+  public user:User;
+  public status: string;
+  public identity;
+  public token;
+  constructor(private _userService : UserService) {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
     this.page_title = 'Ajustes usuario';
+    this.user = new User(this.identity.sub, this.identity.name, this.identity.surname, this.identity.email,'','ROLE_USER','');
   }
 
   ngOnInit() {
