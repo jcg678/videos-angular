6533d3343993cb31e43385a927f16b9c60dfe1f3
jcg678 <jcg678@hotmail.es>
2020-05-12 15:30:57 +0200

    Config component new Video

--- src/app/components/video-new/video-new.component.ts
+++ src/app/components/video-new/video-new.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {UserService} from '../../services/user.service';
+import {Video} from '../../models/video';
 
 @Component({
   selector: 'app-video-new',
   templateUrl: './video-new.component.html',
-  styleUrls: ['./video-new.component.css']
+  styleUrls: ['./video-new.component.css'],
+  providers: [UserService]
 })
 export class VideoNewComponent implements OnInit {
   public page_title:string;
-  constructor() {
+  public identity;
+  public token;
+  public video: Video;
+
+  constructor(private _userService: UserService,
+              private _router: Router,
+              private _route: ActivatedRoute
+  ) {
     this.page_title = 'Nuevo video';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
   ngOnInit() {
+    this.video = new Video(1,this.identity.sub,'','','','',null,null);
   }
+
 
 }
