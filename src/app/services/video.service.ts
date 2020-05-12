b8c614c974ec5f2d7e1d03c4449a30daad73ffc0
jcg678 <jcg678@hotmail.es>
2020-05-12 15:53:23 +0200

    Metodos del servicio de videos

---
+++ src/app/services/video.service.ts
@@ -0,0 +1,50 @@
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { Video} from '../models/video';
+import { global} from './global';
+
+@Injectable()
+export class VideoService {
+  public url: string ;
+
+
+  constructor(public _http: HttpClient) {
+    this.url = global.url;
+  }
+
+  create(token, video): Observable<any> {
+    const json = JSON.stringify(video);
+    const params = 'json=' + json;
+    const headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+    return this._http.post(this.url + 'video/new', params,{ headers : headers });
+  }
+
+  getVideos(token): Observable<any> {
+    const headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+    return this._http.get(this.url + 'video/list', params,{ headers : headers });
+  }
+
+  getVideo(token, id): Observable<any> {
+    const headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+    return this._http.get(this.url + 'video/detail/' + id, params,{ headers : headers });
+  }
+
+  update(token, video, id): Observable<any> {
+    const json = JSON.stringify(video);
+    const params = 'json=' + json;
+    const headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+    return this._http.post(this.url + 'video/edit/' + id, params,{ headers : headers });
+  }
+
+  delete(token, id): Observable<any> {
+    const headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+    return this._http.delete(this.url + 'video/remove/' + id, params,{ headers : headers });
+  }
+
+}
